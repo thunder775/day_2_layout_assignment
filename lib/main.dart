29322de27b524327695bf8f4fc02b88c382e7f25
rahul masih <rahulmasih61@gmail.com>
2019-10-01 14:49:13 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  Container red = Container(
+    height: double.infinity,
+    width: 100,
+    color: Colors.red,
+  );
+  Container yellow = Container(
+    height: 100,
+    width: 100,
+    color: Colors.yellow,
+    margin: EdgeInsets.fromLTRB(50, 0, 50, 0),
+  );
+  Container green = Container(
+    height: 100,
+    width: 100,
+    color: Colors.green,
+    margin: EdgeInsets.fromLTRB(50, 0, 50, 0),
+  );
+
+  Container blue = Container(
+    height: double.infinity,
+    width: 100,
+    color: Colors.lightBlue,
+  );
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(),
+        backgroundColor: Colors.purple,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              red,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [yellow, green],
+              ),
+              blue,
+            ],
+          ),
+        ),
       ),
     ),
   );
